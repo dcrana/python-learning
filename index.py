e4c23da81dceb0c8b2b29f39d85b399063fcc907
dcrana <divyarajsinhrana2487@gmail.com>
2025-04-16 18:12:47 +0530

    loop

---
+++ index.py
@@ -0,0 +1,11 @@
+number = int(input('Enter Number :'))
+
+if number >=0 : 
+    print('greater than equal 0')
+    print('positive')
+else:
+    print('less than 0')
+    print('negative')
+    
+    print('less')
+print('done')
