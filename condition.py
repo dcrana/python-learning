60ce5922b56b66e3f8587015be3d16ce3fda898f
dcrana <divyarajsinhrana2487@gmail.com>
2025-04-18 12:53:10 +0530

    vowel check

--- condition.py
+++ condition.py
@@ -1,10 +1,22 @@
-#  check odd even
-num = int(input('Enter Numner'))
+#  check odd even #
+# num = int(input('Enter Numner'))
 
-if num%2 == 0:
-    print('even')
+# if num%2 == 0:
+#     print('even')
+# else:
+#     print('odd')
+
+#check vowel #
+
+char = input('Enter a character: ').lower()
+
+if len(char) == 1 and char.isalpha():
+    if char in ['a', 'e', 'i', 'o', 'u']:
+        print('It\'s a vowel')
+    else:
+        print('It\'s a consonant')
 else:
-    print('odd')
+    print('Please enter a single alphabet character.')
 
 
 
