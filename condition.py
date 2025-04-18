ba10d194f2fb7c012467c3c059848475c1bef536
dcrana <divyarajsinhrana2487@gmail.com>
2025-04-18 13:00:18 +0530

    check number type

--- condition.py
+++ condition.py
@@ -1,4 +1,4 @@
-#  check odd even #
+#1).check odd even #
 # num = int(input('Enter Numner'))
 
 # if num%2 == 0:
@@ -6,18 +6,27 @@
 # else:
 #     print('odd')
 
-#check vowel #
+#2).check vowel #
+# char = input('Enter a character: ').lower()
 
-char = input('Enter a character: ').lower()
+# if len(char) == 1 and char.isalpha():
+#     if char in ['a', 'e', 'i', 'o', 'u']:
+#         print('It\'s a vowel')
+#     else:
+#         print('It\'s a consonant')
+# else:
+#     print('Please enter a single alphabet character.')
 
-if len(char) == 1 and char.isalpha():
-    if char in ['a', 'e', 'i', 'o', 'u']:
-        print('It\'s a vowel')
-    else:
-        print('It\'s a consonant')
+#3). Check positive , negative , zero
+
+num = int(input('Enter Number to Check:'))
+
+if num == 0:
+    print('Zero')
+elif num > 0:
+    print('Positive')
 else:
-    print('Please enter a single alphabet character.')
-
+    print('Negative')
 
 
 
