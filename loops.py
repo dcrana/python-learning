575d8fe4780dee9e12524044251f00043d2efbf5
dcrana <divyarajsinhrana2487@gmail.com>
2025-04-23 18:17:37 +0530

    for loop

--- loops.py
+++ loops.py
@@ -1,12 +1,21 @@
 #while
 # program to continue sum with 1 using while loop and exit loop while user enters value to 0
+# num = 1
+# sum = 0
 
-num = 1
-sum = 0
+# while num != 0  :
+#     num = int(input('Enter Number : '))
+#     sum = sum + num
+#     print(f'Sum = {sum}')
+# else :
+#     print('Loop Completed')
 
-while num != 0  :
-    num = int(input('Enter Number : '))
-    sum = sum + num
-    print(f'Sum = {sum}')
-else :
-    print('Loop Completed')
+# for
+A = [1,2,5,7,8,345,4]
+B = (5,4,62,3523,34)
+C = 'Divyaraj'
+D = [{"name":'raj',"age" :42},{"name":'raj',"age" :42}]
+E = {"name":'raj',"age" :42}
+R = range(1,10,5)
+for i in R :
+    print(i)
